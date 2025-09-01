72dd32f7a61f35cb468e0cf407f466b387b4c81a
Ferdinand Saurenbach <ferdinand161@outlook.de>
2025-09-01 22:01:08 +0200

    update build script and add build guide!

---
+++ HOW-TO-BUILD.md
@@ -0,0 +1,4 @@
+# How to build KorGE Forge?
+1. Clone korge-forge plugin: git clone https://github.com/korlibs/korge-forge-plugin.git
+2. ./gradlew doForgeRelease in korge-forge-plugin
+3. Execute ./build_forge.sh
--- build_forge.sh
+++ build_forge.sh
@@ -1,4 +1,18 @@
-echo 'Build enviroment'
-docker build . --target intellij_idea --tag intellij_idea
-echo 'Build'
-docker run --rm --user "$(id -u)" --volume "${PWD}:/community" intellij_idea -Dintellij.build.target.os=linux
+#!/usr/bin/env bash
+set -e
+export JAVA_HOME=/home/sauron/.jdks/jbr-21.0.8
+export PATH=$JAVA_HOME/bin:$PATH
+BUILD_ONLY_CURRENT_PLATFORM=true
+
+echo 'Starting KorGE Forge build script!'
+
+if [ "$BUILD_ONLY_CURRENT_PLATFORM" = "true" ]; then
+  echo 'Starting building for current platform...'
+  ./installers.cmd -Dintellij.build.target.os=current
+else
+  echo 'Starting building for all platforms...'
+  ./installers.cmd
+fi
+
+echo 'Find the built KorGE Forge in out/korgeforge/artifacts/ and extract it'
+echo 'Run with ./korge.sh in bin/'
